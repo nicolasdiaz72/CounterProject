d795d2311d4e9c7d28baaed4a32df6327128ba36
Nicolas Diaz <nicodiaz9860@gmail.com>
2023-01-03 16:42:19 -0500

    First

---
+++ app.js
@@ -0,0 +1,35 @@
+// Set initial count
+let count = 0; 
+
+const btns = document.querySelectorAll('.btn ')
+const value = document.querySelector('#value')
+
+btns.forEach(function(btn){
+btn.addEventListener('click', function(e){
+const styles = e.currentTarget.classList
+
+    // Set value to btns
+    if (styles.contains('decrease')){
+        count--;
+    }
+    else if (styles.contains('increase')){
+        count++
+    }
+    else{
+        count = 0;
+
+    // Set color attribute
+    }
+    if (count<0){
+     value.style.color = 'red'
+    }
+    if (count > 0){
+    value.style.color = "green"
+    }
+    if (count === 0){
+    value.style.color = "grey"
+    }
+    value.textContent = count;
+    })
+
+})
